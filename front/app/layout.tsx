21f1875570a998c46369e2e7d08091b6b3a46f39
Agustina Mena <agusmena60@gmail.com>
2026-09-11 14:27:01 -0300

    feat:Agrego margenes

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
     <html lang="es">
       <body className="flex min-h-screen flex-col">
         <Navbar />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 px-6 sm:px-8 lg:px-12">{children}</main>
         <Footer />
       </body>
     </html>
